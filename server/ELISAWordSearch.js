c4f4e4460f8fb5af76096cd65280564fdf9d2e4a
alyssa <alysssachung@gmail.com>
2017-11-09 12:51:45 +0800

    11/6

---
+++ server/ELISAWordSearch.js
@@ -0,0 +1,33 @@
+wordSearch = function(msg, engLexicon) {
+  var wordSearchResult = "";
+  if(msg.indexOf("Which 10") > -1)
+  {
+    var allMatches;
+    if(msg.indexOf("verb")>-1)
+    {
+      allMatches = engLexicon.find({POS:"verb"});
+    }
+    else if(msgt.indexOf("noun")>-1)
+    {
+      allMatches = engLexicon.find({POS:"noun"});
+    }
+    else if(msgt.indexOf("adjective")>-1)
+    {
+      allMatches = engLexicon.find({POS:"adjective"});
+    }
+    if (allMatches !== undefined)
+    {
+      allMatches = allMatches.fetch();
+      for(index=0; index<10 ; index++)
+      {
+        wordSearchResult = wordSearchResult+allMatches[index].Word+",";
+      }
+      wordSearchResult = "There you go honey: "+wordSearchResult;
+    }
+    else
+    {
+      wordSearchResult = "Sorry. Not sorry.";
+    }
+  }
+  return wordSearchResult;
+};
